2ac0b1e84cd61edd88ffd954671216772a04d7a7
Ana María Garzón <63021584+anamarigarzon@users.noreply.github.com>
2023-08-24 14:29:12 -0500

    Create calculadora_facade.py

---
+++ calculadora_facade.py
@@ -0,0 +1,10 @@
+from operation import Operation
+
+class CalculadoraFacade:
+  def __init__(self):
+    pass
+  def execute_binary_operation(self, binary_operation):
+    binary_operation.operate()
+    pass
+  def execute_unary_operation(self, unary_operation):
+    pass
